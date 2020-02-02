5628273821e102e65e023e8199c71ca5b5063e23
Tansu Daylan <tansu.daylan@gmail.com>
2020-02-02 13:09:14 -0500

    NA

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ from allesfitter import config
 from tdpy.util import summgene
 import astropy
 import ttvr.util
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+import numpy.random
+from scipy import stats
+import os, datetime
+import allesfitter
+
+import emcee
+import h5py
+from astropy.coordinates import SkyCoord
+#from tessmaps.src import get_time_on_silicon as gts
+from allesfitter import config
+
+
 print 'Known TESS planet pipeline started.'
 
 if len(sys.argv) == 1:
@@ -266,4 +282,305 @@ elif sys.argv[1] == 'plot':
     plt.close()
     
     ## known planets on the map 
+   
+
+
+# plot target locations using tessmaps
+
+def main():
+   
+    '''
+    Pipeline to analyze previously known planets detected by TESS
+    '''
     
+    # set the numpy random seed
+    np.random.seed(0)
+    
+    # global object
+    gdat = gdatstrt()
+
+    # base data path for the TESS known planet project
+    gdat.pathdata = os.environ['TESS_KNWN_DATA_PATH']
+    os.system('mkdir -p %s' % gdat.pathdata)
+    
+    os.system('mkdir -p %s' % (gdat.pathdata + '/postproc/'))
+    gdat.strgtimestmp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+    
+    print 'Previously known characterization pipeline for TESS started at %s...' % gdat.strgtimestmp
+        
+    # list of known planet names
+    liststrgplan = [ \
+                    #'hats-3', \
+                    'hats-13', \
+                   ]
+    
+    print 'Will analyze the following known exoplanets:'
+    for strgplan in liststrgplan:
+        print strgplan
+    print
+
+    # threshold p value to conclude significant difference between posteriors with and without TESS
+    pvalthrs = 1e-6
+    
+    indxcomp = np.arange(3)
+    
+    # list of rtyp
+    # 0 : wout
+    # 1 : with
+    # 2 : only
+    # list of comp
+    # wout with
+    # wout only
+    # with only
+    indxrtypfrst = [0, 0, 1]
+    indxrtypseco = [1, 2, 2]
+    
+    liststrgrtyp = ['wout', 'with', 'only'] 
+    numbrtyp = len(liststrgrtyp)
+    indxrtyp = np.arange(numbrtyp)
+    #demo_kosm(gdat)
+    
+    lpos = [[] for i in indxrtyp]
+    chan = [[] for i in indxrtyp]
+    lablpara = [[] for i in indxrtyp]
+    numbwalk = [[] for i in indxrtyp]
+    numbswep = [[] for i in indxrtyp]
+    numbsamp = [[] for i in indxrtyp]
+    numbburn = [[] for i in indxrtyp]
+    factthin = [[] for i in indxrtyp]
+    numbpara = [[] for i in indxrtyp]
+    numbplan = len(liststrgplan)
+    indxplan = np.arange(numbplan)
+    for k in indxplan:
+        
+        print 'Processing %s...' % liststrgplan[k] 
+        
+        pathdataplan = gdat.pathdata + liststrgplan[k] + '/'
+    
+        for i, strgrtyp in enumerate(liststrgrtyp):
+            pathdata = pathdataplan + '%stess_mcmc/' % strgrtyp
+            print 'Reading from %s...' % pathdata
+            config.init(pathdata)
+            lablpara[i] = config.BASEMENT.fitlabels
+            print 'config.BASEMENT.fitkeys'
+            print config.BASEMENT.fitkeys
+
+            pathsave = pathdata + 'results/mcmc_save.h5'
+            if not os.path.exists(pathsave):
+                # sample from the posterior excluding the TESS data
+                print 'Calling allesfitter to fit the data...'
+                allesfitter.mcmc_fit(pathdata)
+            
+            emceobjt = emcee.backends.HDFBackend(pathsave, read_only=True)
+            chan[i] = emceobjt.get_chain()
+            
+            # parse configuration
+            numbswep[i] = config.BASEMENT.settings['mcmc_total_steps']
+            numbburn[i] = config.BASEMENT.settings['mcmc_burn_steps']
+            factthin[i] = config.BASEMENT.settings['mcmc_thin_by']
+            numbpara[i] = config.BASEMENT.ndim
+            
+            numbwalk[i] = chan[i].shape[1]
+            numbswep[i] = chan[i].shape[0] * factthin
+            lpos[i] = emceobjt.get_log_prob()
+            numbsamp[i] = lpos[i].size
+            chan[i] = chan[i].reshape((-1, numbpara[i]))
+            print 'Found %d samples and %d parameters...' % (chan[i].shape[0], chan[i].shape[1])
+            print
+
+        lablparacomp = [[] for u in indxcomp]
+        for u in indxcomp:
+            
+            lablparacomp[u] = list(set(lablpara[indxrtypfrst[u]]).intersection(lablpara[indxrtypseco[u]]))
+
+            # post-processing
+            ## calculate the KS test statistic between the posteriors
+            numbparacomp = len(lablparacomp[u])
+            pval = np.empty(numbparacomp)
+            for j in range(numbparacomp):
+                kosm, pval[j] = stats.ks_2samp(chan[indxrtypfrst[u]][:, j], chan[indxrtypseco[u]][:, j])
+
+            ## find the list of parameters whose posterior with and without TESS are unlikely to be drawn from the same distribution
+            indxparagood = np.where(pval < pvalthrs)[0]
+            if indxparagood.size > 0:
+            
+                figr, axis = plt.subplots(figsize=(12, 5))
+                indxparacomp = np.arange(numbparacomp)
+                axis.plot(indxparacomp, pval, ls='', marker='o')
+                axis.plot(indxparacomp[indxparagood], pval[indxparagood], ls='', marker='o', color='r')
+                axis.set_yscale('log')
+                axis.set_xticks(indxparacomp)
+                axis.set_xticklabels(lablparacomp[u])
+                if u == 0:
+                    axis.set_title('Posteriors with TESS vs. without TESS')
+                if u == 1:
+                    axis.set_title('Posteriors without TESS vs. only TESS')
+                if u == 2:
+                    axis.set_title('Posteriors with TESS vs. only TESS')
+
+                axis.axhline(pvalthrs, ls='--', color='black', alpha=0.3)
+                plt.tight_layout()
+                path = gdat.pathdata + 'postproc/%s_kosm_com%d_%s.png' % (gdat.strgtimestmp, u, liststrgplan[k])
+                print 'Writing to %s...' % path
+                figr.savefig(path)
+                plt.close()
+        
+        indxrtypmaxm = 1
+        
+        lablparatotl = np.unique(np.concatenate(lablpara))
+
+        for l, lablparatemp in enumerate(lablparatotl):
+            figr, axis = plt.subplots()
+            numbbins = 50
+
+            listindxrtypcomp = []
+            for i in indxrtyp:
+                if lablparatemp in lablpara[i]:
+                    listindxrtypcomp.append(i)
+            
+            #minm = 1e99
+            #maxm = -1e99
+            #for i in listindxrtypcomp:
+            #    m = np.where(lablpara[i] == lablparatemp)[0][0]
+            #    minm = min(minm, np.amin(chan[i][:, m]))
+            #    maxm = max(maxm, np.amax(chan[i][:, m]))
+            #bins = np.linspace(minm, maxm, numbbins + 1)
+            ## list of comp
+            ## wout with
+            ## wout only
+            ## with only
+            #for i in listindxrtypcomp:
+            #    m = np.where(lablpara[i] == lablparatemp)[0][0]
+            #    if i == 0:
+            #        labl = 'w/o TESS'
+            #    if i == 1:
+            #        labl = 'with TESS'
+            #    if i == 2:
+            #        labl = 'only TESS'
+            #    axis.hist(chan[i][:, m], alpha=0.4, lw=10, histtype='stepfilled', bins=bins, label=labl)
+            #axis.set_xlabel(lablparatemp) 
+            #axis.legend()
+            #plt.tight_layout()
+            #path = gdat.pathdata + 'postproc/pdfn_pr%02d_%s.png' % (l, liststrgplan[k])
+            #print 'Writing to %s...' % path
+            #figr.savefig(path)
+            #plt.close()
+            
+            figr, axis = plt.subplots()
+            chanlist = []
+            for i in listindxrtypcomp:
+                m = np.where(lablpara[i] == lablparatemp)[0][0]
+                
+                if lablparatemp == '$T_{0;b}$':
+                    offs = np.mean(chan[i][:, m])
+                    chantemp = chan[i][:, m] - offs
+                    print 'Subtracting %g from T_b for TESS...' % offs
+                else:
+                    chantemp = chan[i][:, m]
+
+                chanlist.append(chantemp)
+            axis.violinplot(chanlist, showmeans=True)
+            
+            if listindxrtypcomp == [0, 1]:
+                labltick = ['w/o TESS', 'w/ TESS']
+            if listindxrtypcomp == [0, 2]:
+                labltick = ['w/o TESS', 'only TESS']
+            if listindxrtypcomp == [1, 2]:
+                labltick = ['w/ TESS', 'only TESS']
+            if listindxrtypcomp == [0, 1, 2]:
+                labltick = ['w/o TESS', 'w/ TESS', 'only TESS']
+            valutick = np.arange(len(labltick)) + 1
+
+            axis.set_xticks(valutick)
+            axis.set_xticklabels(labltick)
+            axis.set_ylabel(lablparatemp)
+            plt.tight_layout()
+
+            path = gdat.pathdata + 'postproc/%s_viol_pr%02d_%s.png' % (gdat.strgtimestmp, l, liststrgplan[k])
+            print 'Writing to %s...' % path
+            figr.savefig(path)
+            plt.close()
+        
+        #figr, axis = plt.subplots()
+        #chanlist = []
+        #for i, strglabl in []:
+        #    m = np.where(lablpara[i] == lablparatemp)[0][0]
+        #    if i == 2 and lablparatemp == '$T_{0;b}$':
+        #        offs = np.mean(chan[i][2, m] - chan[0][:, m])
+        #        chantemp = chan[i][:, m] - offs 
+        #        print 'Subtracting %g from T_b for TESS...' % offs
+        #    else:
+        #        chantemp = chan[i][:, m]
+
+        #    chanlist.append(chantemp)
+        #axis.violinplot(chanlist, showmeans=True)
+        #
+        #if listindxrtypcomp == [0, 1]:
+        #    labltick = ['w/o TESS', 'w/ TESS']
+        #if listindxrtypcomp == [0, 2]:
+        #    labltick = ['w/o TESS', 'only TESS']
+        #if listindxrtypcomp == [1, 2]:
+        #    labltick = ['w/ TESS', 'only TESS']
+        #if listindxrtypcomp == [0, 1, 2]:
+        #    labltick = ['w/o TESS', 'w/ TESS', 'only TESS']
+        #valutick = np.arange(len(labltick)) + 1
+
+        #axis.set_xticks(valutick)
+        #axis.set_xticklabels(labltick)
+        #axis.set_ylabel(lablparatemp)
+        #plt.tight_layout()
+
+        #path = gdat.pathdata + 'postproc/%s_viol_pr%02d_%s.png' % (gdat.strgtimestmp, l, liststrgplan[k])
+        #print 'Writing to %s...' % path
+        #figr.savefig(path)
+        #plt.close()
+        
+
+def retr_listexop(gdat):
+   
+    pass
+
+    # temp
+    #coords = SkyCoord(['124.532 -68.313', '42.42, -42.42'], unit='deg')
+    #df = gts.get_time_on_silicon(coords)
+    #print 'Time on silicon'
+    #print 'df'
+    #print df
+    
+
+def demo_kosm(gdat):
+    
+    '''
+    Demonstrates Kolmogorov-Smirnov test statistics
+    '''
+     
+    path = gdat.pathdata + 'plt.png'
+    
+    if not os.path.exists(path):
+        numbsamp = 1000
+        numbpara = 4
+        indxpara = np.arange(numbpara)
+        
+        shft = 1.5 * np.random.randn(numbpara)
+        shrk = 0.5 * np.random.randn(numbpara) + 1.
+        
+        chan = np.random.randn(numbpara * numbsamp).reshape((numbsamp, numbpara))
+        for k in indxpara:
+            chan[:, k] = shft[k] + np.random.randn(numbsamp) / shrk[k]
+        
+        strgtitl = ''
+        figr, axis = plt.subplots()
+        for k in indxpara:
+            plt.hist(chan[:, k], alpha=0.4, lw=10, histtype='stepfilled')
+            if k != 0:
+                kosm, pval = stats.ks_2samp(chan[:, 0], chan[:, k])
+                strgtitl += ' %.5g' % pval
+        axis.set_title(strgtitl)
+        plt.tight_layout()
+        figr.savefig(path)
+        plt.close()
+
+allesfitter.post_proc(pathdata)
+#demo_kosm()
+
+
